887bb6793d5dc72bc70ae61656c01a571f67a679
Yuki Horikoshi <y.horikoshi.pg@gmail.com>
2021-07-21 05:18:38 +0000

    loading false

--- src/app.js
+++ src/app.js
@@ -1,27 +1,9 @@
-import { LightningElement } from "lwc";
-
+import { LightningElement } from 'lwc';
 export default class App extends LightningElement {
-  title = "Welcome to Lightning Web Components!";
-
-  showFeatures = true;
-
-  /**
-   * Getter for the features property
-   */
-  get features() {
-    return [
-      {
-        label: "Learn in the browser.",
-        icon: "utility:edit",
-      },
-      {
-        label: "View changes to code instantly with Live Compilation.",
-        icon: "utility:refresh",
-      },
-      {
-        label: "Style your components with SLDS.",
-        icon: "utility:brush",
-      },
-    ];
-  }
+   name = 'Electra X4';
+   description = 'A sweet bike built for comfort.';
+   category = 'Mountain';
+   material = 'Steel';
+   price = '$2,700';
+   pictureUrl = 'https://s3-us-west-1.amazonaws.com/sfdc-demo/ebikes/electrax4.jpg';
 }
